d3493e4a0aa924bf07b4e3150489970357e4a2bd
Phạm Dũng <phamdung@192.168.2.69>
2025-06-20 22:41:37 +0700

    fix  getStaticPaths

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -75,7 +75,7 @@ export default function PostDetail({ post }: { post: Post }) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // Pre-render 5 popular posts at build time
-  const res = await fetch("https://dev.to/api/articles?top=50");
+  const res = await fetch("https://dev.to/api/articles");
   const posts = await res.json();
 
   const paths = posts.map((post: Post) => ({
